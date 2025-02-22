7f195b6102343956d1dedd5d77106c46bbd61ebe
Henrique <hcoelhosev@hotmail.com>
2025-02-22 20:16:40 -0300

    feat: foi adicionado metodos Get Set

--- br/com/mentoria/java/Pessoa.java
+++ br/com/mentoria/java/Pessoa.java
@@ -9,4 +9,44 @@ public class Pessoa {
     private String telefone;
     private String email;
     private StatusCivilEnum statusCivil;
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return this.dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
