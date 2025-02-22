86df20ce4797caf0be618617e2a60c588909a67c
Henrique <hcoelhosev@hotmail.com>
2025-02-22 20:11:40 -0300

    feat: foi adicionado a classe Pessoa

---
+++ br/com/mentoria/java/Pessoa.java
@@ -0,0 +1,12 @@
+package br.com.mentoria.java;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+    private String cpf;
+    private String nome;
+    private LocalDate dataNascimento;
+    private String telefone;
+    private String email;
+    private StatusCivilEnum statusCivil;
+}
