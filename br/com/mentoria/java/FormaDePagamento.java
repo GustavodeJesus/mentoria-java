3d1709db8d1427ec304b68eb225ae2338b1e51b2
Pedro Henrique <pedrohenrique@MacBook-Pro-de-Pedro.local>
2025-02-25 21:44:26 -0300

    feat: criando enum para os tipos de pagamentos

--- br/com/mentoria/java/FormaDePagamento.java
+++ br/com/mentoria/java/FormaDePagamento.java
@@ -5,4 +5,28 @@ public class FormaDePagamento {
     private String tipodepagamento;
     private int numeroMaximoParcelas;
     private String bandeira;
+
+    public String getTipodepagamento() {
+        return tipodepagamento;
+    }
+
+    public void setTipodepagamento(String tipodepagamento) {
+        this.tipodepagamento = tipodepagamento;
+    }
+
+    public int getNumeroMaximoParcelas() {
+        return numeroMaximoParcelas;
+    }
+
+    public void setNumeroMaximoParcelas(int numeroMaximoParcelas) {
+        this.numeroMaximoParcelas = numeroMaximoParcelas;
+    }
+
+    public String getBandeira() {
+        return bandeira;
+    }
+
+    public void setBandeira(String bandeira) {
+        this.bandeira = bandeira;
+    }
 }
