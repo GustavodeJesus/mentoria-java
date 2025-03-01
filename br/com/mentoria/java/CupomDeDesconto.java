655371aad25cca23ab040bce9a4c2f577a089c85
Pedro Henrique de Souza Pereira <pedrinwmp@gmail.com>
2025-03-01 09:29:18 -0300

    feat: adicionando get e set na clasee

--- br/com/mentoria/java/CupomDeDesconto.java
+++ br/com/mentoria/java/CupomDeDesconto.java
@@ -10,4 +10,32 @@ public class CupomDeDesconto {
     private BigDecimal valorMinimo;
     private LocalDate dataValidade;
 
+
+    public CupomDeDesconto(String codigo,
+                           BigDecimal porcentagem,
+                           BigDecimal valorMinimo,
+                           LocalDate dataValidade) {
+        this.codigo = codigo;
+        this.porcentagem = porcentagem;
+        this.valorMinimo = valorMinimo;
+        this.dataValidade = dataValidade;
+    }
+
+
+    public String getCodigo(){
+        return this.codigo;
+    }
+
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+
+    public BigDecimal getPorcentagem(){
+        return this.porcentagem;
+    }
+
+    public void setPorcentagem(BigDecimal Porcentagem){
+        this.porcentagem = porcentagem;
+    }
+
 }
