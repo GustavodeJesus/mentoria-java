f9bb1845a949a3f58796b75230f123dc5c4c9292
Pedro Henrique de Souza Pereira <pedrinwmp@gmail.com>
2025-03-01 09:17:58 -0300

    feat: adicionado atributos a classe

---
+++ br/com/mentoria/java/CupomDeDesconto.java
@@ -0,0 +1,13 @@
+package br.com.mentoria.java;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class CupomDeDesconto {
+
+    private String codigo;
+    private BigDecimal porcentagem;
+    private BigDecimal valorMinimo;
+    private LocalDate dataValidade;
+
+}
