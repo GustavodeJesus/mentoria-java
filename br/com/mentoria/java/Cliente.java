fc42afaf1fcab2e16f63baa602438978bbca7542
natamaraes <nataliaafborges@hotmail.com>
2025-03-06 20:14:10 -0300

    feat: criado classe cliente

---
+++ br/com/mentoria/java/Cliente.java
@@ -0,0 +1,53 @@
+package br.com.mentoria.java;
+
+import java.time.LocalDate;
+
+public class Cliente {
+
+    private String codigoCliente;
+    private int numeroPedidos;
+    private boolean possuiRestricoes;
+    private final LocalDate dataCadastro;
+
+    public Cliente(
+            String codigoCliente,
+            int numeroPedidos,
+            boolean possuiRestricoes,
+            LocalDate dataCadastro){
+        this.codigoCliente = codigoCliente;
+        this.numeroPedidos = numeroPedidos;
+        this.possuiRestricoes = possuiRestricoes;
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getCodigoCliente() {
+        return this.codigoCliente;
+    }
+
+    public void setCodigoCliente(String codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public int getNumeroPedidos() {
+        return this.numeroPedidos;
+    }
+
+    public void setNumeroPedidos(int numeroPedidos) {
+        this.numeroPedidos = numeroPedidos;
+    }
+
+    public boolean getPossuiRestricoes() {
+        return this.possuiRestricoes;
+    }
+
+    public void setPossuiRestricoes(boolean possuiRestricoes) {
+        this.possuiRestricoes = possuiRestricoes;
+    }
+
+    public LocalDate getDataCadastro() {
+        return this.dataCadastro;
+    }
+
+
+
+}
