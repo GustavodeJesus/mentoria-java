01620188ec1089163e6c80a14ee0cbf0732b679e
opedroasa <pedroantonio2502@gmail.com>
2025-05-18 12:02:40 -0300

    task: Criar formulario cadastro produto

--- src/main/java/br/com/mentoriajava/database/ProdutoDataSource.java
+++ src/main/java/br/com/mentoriajava/database/ProdutoDataSource.java
@@ -1,6 +1,7 @@
 package br.com.mentoriajava.database;
 
 import br.com.mentoriajava.produtos.Produto;
+import br.com.mentoriajava.produtos.ProdutoCategoria;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -14,18 +15,25 @@ public class ProdutoDataSource {
 
     }
 
+    //Lista de Categorias
+    private final ObservableList<ProdutoCategoria> listaDeCategorias = FXCollections.observableArrayList(
+            new ProdutoCategoria ("Perfume"),
+            new ProdutoCategoria("Creme"),
+            new ProdutoCategoria("Sabonete")
+    );
+
     //Lista de Produtos
     private final ObservableList<Produto> listaDeProdutos = FXCollections.observableArrayList(
-            new Produto ("Kaiak 100ML" , "Perfume" , 150.9, 15),
-            new Produto ("Hidratante Flor de Cerejeira 250ML" , "Creme" , 99.90, 12),
-            new Produto ("Sabonete Vegano Cereja UN" , "Sabonete", 4.90, 89),
-            new Produto ("Ilia 50ML" , "Perfume", 185.9, 22),
-            new Produto( "Essencial Fem 120ML" , "Perfume" , 179.9 , 6),
-            new Produto ("Creme Nutritivo Ameixa 400ML" , "Creme" , 44.9 , 55),
-            new Produto("Sabonete Vegano Alecrim UN" , "Sabonete", 4.90, 52),
-            new Produto("Sabonete Vegano Amora UN" , "Sabonete", 4.90, 13),
-            new Produto("Una 75ML", "Perfume" , 219.90 , 3),
-            new Produto("Hidratante Baunilha 250ML" , "Creme", 99.9,9)
+            new Produto ("Kaiak 100ML" , new ProdutoCategoria("Perfume"), 150.9, 15),
+            new Produto ("Hidratante Flor de Cerejeira 250ML" , new ProdutoCategoria("Creme") , 99.90, 12),
+            new Produto ("Sabonete Vegano Cereja UN" , new ProdutoCategoria("Sabonete"), 4.90, 89),
+            new Produto ("Ilia 50ML" , new ProdutoCategoria("Perfume"), 185.9, 22),
+            new Produto( "Essencial Fem 120ML" , new ProdutoCategoria("Perfume") , 179.9 , 6),
+            new Produto ("Creme Nutritivo Ameixa 400ML" , new ProdutoCategoria("Creme") , 44.9 , 55),
+            new Produto("Sabonete Vegano Alecrim UN" , new ProdutoCategoria("Sabonete"), 4.90, 52),
+            new Produto("Sabonete Vegano Amora UN" , new ProdutoCategoria("Sabonete"), 4.90, 13),
+            new Produto("Una 75ML", new ProdutoCategoria("Perfume") , 219.90 , 3),
+            new Produto("Hidratante Baunilha 250ML" , new ProdutoCategoria("Creme"), 99.9,9)
     );
 
     //Métodos
@@ -35,6 +43,10 @@ public class ProdutoDataSource {
 
     public ObservableList<Produto> getListaDeProdutos() {
         return listaDeProdutos;
+    }
+
+    public ObservableList<ProdutoCategoria> getListaDeCategorias(){
+        return listaDeCategorias;
     }
 
     public void adicionarProduto (Produto produto){
--- src/main/java/br/com/mentoriajava/produtos/ProdutosScreen.java
+++ src/main/java/br/com/mentoriajava/produtos/ProdutosScreen.java
@@ -1,28 +1,38 @@
 package br.com.mentoriajava.produtos;
 
+import br.com.mentoriajava.database.ProdutoDataSource;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.util.StringConverter;
 
 public class ProdutosScreen extends VBox {
 
     private final TextField campoNome = new TextField();
-    private final TextField campoCategoria = new TextField();
     private final TextField campoPreco = new TextField();
     private final TextField campoEstoque = new TextField();
+    private final ComboBox<ProdutoCategoria> comboBoxCategoria = new ComboBox<>();
 
 
     public ProdutosScreen() {
+        configurarLayout();
+    }
+
+    private void configurarLayout()
+    {
         setSpacing(20);
         setPadding(new Insets(20));
         Label title = new Label("Tela de Produtos");
         title.setFont(Font.font("System",javafx.scene.text.FontWeight.BOLD,20));
         title.setStyle("-fx-text-fill: #1f2937;");
+        setStyle("-fx-background-color: #ffffff;");
 
 
         VBox secaoFormulario = construirFormulario();
@@ -35,8 +45,12 @@ public class ProdutosScreen extends VBox {
         containerFormulario.setStyle("-fx-background-color: #f9fafb; -fx-border-color: #e5e7eb; -fx-border-radius: 8; -fx-background-radius: 8;");
 
         GridPane grid = configurarGridFormulario();
+        Button botaoCadastrar = criarBotaoCadastrar();
 
-        containerFormulario.getChildren().addAll(grid);
+        HBox boxBotao = new HBox(botaoCadastrar);
+        boxBotao.setAlignment(Pos.CENTER_RIGHT);
+
+        containerFormulario.getChildren().addAll(grid,boxBotao);
         return containerFormulario;
     }
 
@@ -48,14 +62,28 @@ public class ProdutosScreen extends VBox {
 
         campoNome.setPromptText("Nome Produto");
         campoNome.setPrefWidth(250);
-        campoCategoria.setPromptText("Categoria");
+        comboBoxCategoria.setPromptText("Categoria");
+        comboBoxCategoria.setItems(ProdutoDataSource.getInstanciaProduto().getListaDeCategorias());
+        comboBoxCategoria.setConverter(new StringConverter<>(){
+
+            @Override
+            public String toString(ProdutoCategoria produtoCategoria) {
+                return produtoCategoria.getDescricao();
+            }
+
+            @Override
+            public ProdutoCategoria fromString(String s) {
+                return null;
+            }
+        });
         campoPreco.setPromptText("Preço");
         campoEstoque.setPromptText("Estoque");
 
         grid.add(new Label("Nome:"),0,0);
         grid.add(campoNome,1,0);
         grid.add(new Label("Categoria:"),2,0);
-        grid.add(campoCategoria,3,0);
+        grid.add(comboBoxCategoria,3,0);
+
 
         grid.add(new Label("Preço:"),0,1);
         grid.add(campoPreco,1,1);
@@ -65,5 +93,23 @@ public class ProdutosScreen extends VBox {
         return grid;
     }
 
+    private Button criarBotaoCadastrar()
+    {
+        Button botaoCadastrar = new Button("Cadastrar Produto");
+        botaoCadastrar.setStyle("-fx-background-color: #3b82f6; -fx-text-fill: white; -fx-padding: 10 20; -fx-font-weight: bold; -fx-background-radius: 6;");
+        //botaoCadastrar.setOnAction(event -> cadastrarProduto()); **CRIAR METODO
+        return botaoCadastrar;
+    }
+
+  /* private void cadastrarProduto()
+    {
+        String nome = campoNome.getText();
+        ProdutoCategoria categoria = comboBoxCategoria.getValue();
+        Double preco = Double.parseDouble(campoPreco.getText());
+        Long estoque = Long.parseLong(campoEstoque.getText());
+
+        **FINALIZAR
+
+    }*/
 
 }
