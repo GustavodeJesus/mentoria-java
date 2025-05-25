0b771797c7e60f905027f47443464d4cf9d0931b
Henrique <hcoelhosev@hotmail.com>
2025-05-25 11:29:17 -0300

    Corrigindo coments

--- src/main/java/br/com/mentoriajava/exemplo/PetScreen.java
+++ src/main/java/br/com/mentoriajava/exemplo/PetScreen.java
@@ -205,7 +205,7 @@ public class PetScreen extends VBox {
     }
 
     /**
-     * Aponta a tabela para a lista de Vendedores observável.
+     * Aponta a tabela para a lista de Pets observável.
      */
     private void configurarTabela() {
         tabelaPets.setItems(PetDataSource.getInstancia().getListaDePets());
@@ -296,7 +296,7 @@ public class PetScreen extends VBox {
     }
 
     /**
-     * Remove o Vendedor da linha informada com confirmação do usuário.
+     * Remove o Pets da linha informada com confirmação do usuário.
      */
     private void removerPet(int index) {
         Pet pet = tabelaPets.getItems().get(index);
@@ -313,7 +313,7 @@ public class PetScreen extends VBox {
     }
 
     /**
-     * Atualiza a tabela com os Vendedores do DataSource.
+     * Atualiza a tabela com os Pets do DataSource.
      */
     private void popularTabela() {
         tabelaPets.setItems(PetDataSource.getInstancia().getListaDePets());
