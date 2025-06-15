ae9065b37b1f76472132223f5ab3af803fc2d4fd
Pedro Henrique de Souza Pereira <pedrinwmp@gmail.com>
2025-06-15 11:45:00 -0300

    feat: criacao do layout com VBox, estilo e espacamento

--- src/main/java/br/com/mentoriajava/formaspagamento/FormaPagamentoScreen.java
+++ src/main/java/br/com/mentoriajava/formaspagamento/FormaPagamentoScreen.java
@@ -2,13 +2,26 @@ package br.com.mentoriajava.formaspagamento;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
-public class FormaPagamentoScreen extends StackPane {
+public class FormaPagamentoScreen extends VBox {
+
     public FormaPagamentoScreen() {
-        setPadding(new Insets(20));
-        Label title = new Label("Tela de Forma de Pagamento");
-        title.setStyle("-fx-font-size: 20px; -fx-text-fill: #1f2937;");
-        getChildren().add(title);
+        configurarLayoutPrincipal();
     }
+
+    private void configurarLayoutPrincipal() {
+        setSpacing(20);
+        setPadding(new Insets(20));
+        setStyle("-fx-background-color:#ffffff;");
+
+        Label titulo = new Label("Cadastro de Forma de Pagamento");
+        titulo.setFont(Font.font("System", javafx.scene.text.FontWeight.BOLD, 20));
+        titulo.setStyle("-fx-text-fill: #1f2937;");
+
+        getChildren().addAll(titulo);
+
+    }
+
 }
